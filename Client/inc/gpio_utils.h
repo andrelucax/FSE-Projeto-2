a44b2c0e43f862167d93f65ac5968802ca3da763
Andre Pinto <andrelucax@gmail.com>
2020-10-30 15:52:50 -0300

    added first implementation of Distributed Server

---
+++ Client/inc/gpio_utils.h
@@ -0,0 +1,33 @@
+#ifndef _GPIO_UTILS_H_
+#define _GPIO_UTILS_H_
+
+#include <bcm2835.h>
+
+#define ON 1
+#define OFF 0
+
+#define LAMP 0
+#define AIR 1
+#define PRESENCE 2
+#define OPENNING 3
+
+#define LAMP1 RPI_V2_GPIO_P1_11 // KITCHEN
+#define LAMP2 RPI_V2_GPIO_P1_12 // LIVING ROOM
+#define LAMP3 RPI_V2_GPIO_P1_13 // BEDROOM 1
+#define LAMP4 RPI_V2_GPIO_P1_15 // BEDROOM 2
+#define AIR1 RPI_V2_GPIO_P1_16 // BEDROOM 1
+#define AIR2 RPI_V2_GPIO_P1_18 // BEDROOM 2
+#define PRESENCE1 RPI_V2_GPIO_P1_22 // LIVING ROOM
+#define PRESENCE2 RPI_V2_GPIO_P1_37 // KITCHEN
+#define OPENNING1 RPI_V2_GPIO_P1_29 // KITCHEN DOOR
+#define OPENNING2 RPI_V2_GPIO_P1_31 // KITCHEN WINDOW
+#define OPENNING3 RPI_V2_GPIO_P1_32 // LIVING ROOM DOOR
+#define OPENNING4 RPI_V2_GPIO_P1_36 // LIVING ROOM WINDOW
+#define OPENNING5 RPI_V2_GPIO_P1_38 // BEDROOM 1 WINDOW
+#define OPENNING6 RPI_V2_GPIO_P1_40 // BEDROOM 2 WINDOW
+
+int init_bcm();
+int outp_device_on_off(int device_type, int device_id, int on_off);
+void get_inpt_device_on_off(int presence[], int openning[]);
+
+#endif /* _GPIO_UTILS_H_ */
--- Client/src/main.c
+++ Client/src/main.c
@@ -1,5 +1,60 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+// My Headers
+#include "gpio_utils.h"
+#include "bme280_utils.h"
+
+// Handle signals
+void sig_handler(int signum);
+void update_values();
+void send_data();
 
 int main(){
+    if(init_bcm()){
+        printf("Failed to start bcm\n");
+        exit(-1);
+    }
+
+    if (setup_bme280()){
+        printf("Failed to start bme280\n");
+        exit(-2);
+    }
+	
+    // Get SIGALARM
+    signal(SIGALRM, sig_handler);
+
+    // SIGALARM every 1 sec
+    ualarm(1000000, 1000000);
+
+    sig_handler(SIGALRM);
+	
     return 0;
+}
+
+void sig_handler(int signum){
+    if (signum == SIGALRM){
+        // Every 1 sec
+        double humidity = 0, temperature = 0;
+        int presence[2], openning[6];
+        update_values(&humidity, &temperature, presence, openning);
+        send_data(&humidity, &temperature, presence, openning);
+    }
+}
+
+void update_values(double *humidity, double *temperature, int presence[], int openning[]){
+    get_bme280_temperature_humidity(temperature, humidity);
+
+    get_inpt_device_on_off(presence, openning);
+}
+
+void send_data(double *humidity, double *temperature, int presence[], int openning[]){
+    printf("H %lf\nT %lf\n", *humidity, *temperature);
+    for(int i = 0; i < *(&presence + 1) - presence; i++){
+        printf("P%d %d\n", i+1, presence[i]);
+    }
+    for(int i = 0; i < *(&openning + 1) - openning; i++){
+        printf("O%d %d\n", i+1, openning[i]);
+    }
 }
