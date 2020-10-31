1922365c81613e682d2d50fa3003b4ac24ebbb83
Andre Pinto <andrelucax@gmail.com>
2020-10-31 14:36:36 -0300

    Alarm to send data 1 sec

--- DistributedServer/src/main.c
+++ DistributedServer/src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 // My libs
 #include "gpio_utils.h"
@@ -14,8 +15,12 @@ void sig_handler(int signum);
 void update_values();
 void send_data();
 void before_exit();
+void *handle_send_data();
 
 pthread_t thread_tcp_server;
+pthread_t thread_tcp_client;
+
+sem_t sem_send_data;
 
 int main(){
     printf("Handling signals...\n");
@@ -26,7 +31,7 @@ int main(){
     signal(SIGTERM, sig_handler);
     signal(SIGABRT, sig_handler);
     signal(SIGQUIT, sig_handler);
-    // ualarm(1000000, 1000000);
+    alarm(1);
 
     printf("Starting BCM...\n");
     if(init_bcm()){
@@ -52,6 +57,12 @@ int main(){
         printf("Fail to create TCP server thread");
         exit(-4);
     }
+    if (pthread_create(&thread_tcp_client, NULL, handle_send_data, NULL))
+    {
+        printf("Fail to create TCP server thread");
+        exit(-4);
+    }
+    sem_init(&sem_send_data, 0, 0);
 
     printf("Running\n");
 
@@ -62,13 +73,21 @@ int main(){
     return 0;
 }
 
-void sig_handler(int signum){
-    if (signum == SIGALRM){
-        // Every 1 sec
+void *handle_send_data(){
+    while(1){
+        sem_wait(&sem_send_data);
         double humidity = 0, temperature = 0;
         int presence[2], openning[6];
         update_values(&humidity, &temperature, presence, openning);
         send_data(&humidity, &temperature, presence, openning);
+    }
+}
+
+void sig_handler(int signum){
+    if (signum == SIGALRM){
+        // Every 1 sec
+        alarm(1);
+        sem_post(&sem_send_data);
     }
     else{
         before_exit();
